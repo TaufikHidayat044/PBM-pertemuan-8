377b3be2fe0b426d0cf77f2ab153e07e1082149c
TaufikHidayat044 <youthcloud.id@gmail.com>
2021-11-21 14:44:41 +0700

    Tugas pertemuan 8

---
+++ app/src/main/java/id/taufik/hidayat/adapter/ItemAdapter.kt
@@ -0,0 +1,38 @@
+package id.taufik.hidayat.adapter
+import id.taufik.hidayat.model.Affirmation
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import id.taufik.hidayat.R
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object.
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+}
---
+++ app/src/main/java/id/taufik/hidayat/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package id.taufik.hidayat.model
+
+data class Affirmation(val stringResourceId: Int)
